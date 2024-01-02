63b352584eca6ef21de2acbb362f49f0d7f8bf2d
Maroof-Ali-Qureshi <123650922+Maroof-Ali-Qureshi@users.noreply.github.com>
2024-01-02 13:29:51 +0500

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../firebase-config";
 
 const Home = () => {
-  return <div>Home</div>;
-};
+  const [postLists, setPostList] = useState([]);
+  const postsCollectionRef = collection(db, "posts");
 
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postsCollectionRef);
+      setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getPosts();
+  });
+
+  return (
+    <div className="homePage">
+      {postLists.map((post) => {
+        return <div className="post">{post.title}</div>;
+      })}
+    </div>
+  );
+};
 export default Home;
